4de68b4619392e9a904065f475f00562daa3375f
uveshnizam043 <mohduvesh043@gmail.com>
2025-02-25 19:04:37 +0530

    Set up a project using Pinia and Tailwind CSS.

---
+++ app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="bg-red-600 shadow-md rounded-lg overflow-hidden hover:shadow-lg transition-shadow duration-300 flex flex-col h-full">
+    <div class="p-6 flex-grow">
+      llrem
+      </div>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  modules: ["@pinia/nuxt", "@nuxtjs/tailwindcss"],
+  css: ['~/assets/css/tailwind.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+});
