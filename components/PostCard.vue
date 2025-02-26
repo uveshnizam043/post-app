00a49dab93fc51df4a7c248525206e22601c3fbb
uveshnizam043 <mohduvesh043@gmail.com>
2025-02-26 12:03:42 +0530

    add caching for posts and single post

--- components/PostCard.vue
+++ components/PostCard.vue
@@ -32,7 +32,7 @@
         </div>
         <NuxtLink
       
-          class="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-green-400 rounded-lg hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-blue-500 cursor-pointer"
+          class="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-blue-400 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 cursor-pointer"
         >
           Read More
           <Icon name="uil:arrow-right" size="24" />
--- stores/posts.js
+++ stores/posts.js
@@ -1,3 +1,4 @@
+
 export const usePostsStore = defineStore("posts", {
   state: () => ({
     posts: [],
@@ -8,9 +9,28 @@ export const usePostsStore = defineStore("posts", {
     async fetchPosts() {
       console.log("fetchPosts");
       this.loading = true;
+
+      const cachedPosts = sessionStorage.getItem("posts");
+      const cachedTime = sessionStorage.getItem("posts_timestamp");
+
+      if (cachedPosts && cachedTime) {
+        const currentTime = new Date().getTime();
+        const cacheTime = parseInt(cachedTime);
+        if (currentTime - cacheTime < 900000) {
+          this.posts = JSON.parse(cachedPosts);
+          this.loading = false;
+          return;
+        }
+      }
       try {
         const data = await $fetch("https://dummyjson.com/posts");
         this.posts = data.posts;
+
+        sessionStorage.setItem("posts", JSON.stringify(this.posts));
+        sessionStorage.setItem(
+          "posts_timestamp",
+          new Date().getTime().toString()
+        );
       } catch (error) {
         console.log("error", error);
       } finally {
@@ -20,11 +40,30 @@ export const usePostsStore = defineStore("posts", {
 
     async fetchPostById(id) {
       this.loading = true;
+
+      const cachedPost = sessionStorage.getItem(`post_${id}`);
+      const cachedTime = sessionStorage.getItem(`post_${id}_timestamp`);
+
+      if (cachedPost && cachedTime) {
+        const currentTime = new Date().getTime();
+        const cacheTime = parseInt(cachedTime);
+
+        if (currentTime - cacheTime < 900000) {
+          this.post = JSON.parse(cachedPost);
+          this.loading = false;
+          return;
+        }
+      }
       try {
         const response = await fetch(`https://dummyjson.com/posts/${id}`);
         const data = await response.json();
 
         this.post = data;
+        sessionStorage.setItem(`post_${id}`, JSON.stringify(this.post));
+        sessionStorage.setItem(
+          `post_${id}_timestamp`,
+          new Date().getTime().toString()
+        );
       } catch (error) {
         this.error = error.message;
       } finally {
